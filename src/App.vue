ba47e6ce2b1c1e3b52b45b90c0dbcef39a03900a
arsad33 <mustafa.aang@gmail.com>
2022-02-24 01:41:53 +0300

    Until Delete

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <div id="app">
-   
+  <div id="app" class="container">
+    <div class="row justify-content-center">
+      <appointment-list :appointments="appointments"/>
+    </div>
   </div>
 </template>
 
 <script>
 
+import axios from "axios"
+import AppointmentList from "./components/AppointmentList.vue"
 
 export default {
-  name: 'App',
+  name: "App",
+  data: function () {
+    return {
+      title: "Appointment List",
+      appointments: []
+    };
+  },
   components: {
-    
+    AppointmentList
+  },
+  mounted(){
+    axios.get("./data/appointments.json")
+    .then(response => (this.appointments = response.data))
   }
-}
+};
 </script>
 
 <style>
-
 </style>
