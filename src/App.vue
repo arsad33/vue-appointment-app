eddecba2b3519186e8d6a88f728972327e2e7da2
arsad33 <mustafa.aang@gmail.com>
2022-02-24 03:14:13 +0300

    Edit feature

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,48 @@
 <template>
   <div id="app" class="container">
     <div class="row justify-content-center">
-      <appointment-list :appointments="appointments" @remove="removeItem"/>
+      <appointment-list :appointments="appointments" @remove="removeItem" @edit="editItem"/>
     </div>
   </div>
 </template>
 
 <script>
-
-import axios from "axios"
-import AppointmentList from "./components/AppointmentList.vue"
-import _ from "lodash"
+import axios from "axios";
+import AppointmentList from "./components/AppointmentList.vue";
+import _ from "lodash";
 
 export default {
   name: "App",
   data: function () {
     return {
       appointments: [],
-      aptIndex: 0
+      aptIndex: 0,
     };
   },
   components: {
-    AppointmentList
+    AppointmentList,
   },
-  mounted(){
-    axios.get("./data/appointments.json")
-    .then(response => (this.appointments = response.data))
+  mounted() {
+    axios.get("./data/appointments.json").then(
+      (response) =>
+        (this.appointments = response.data.map((item) => {
+          item.aptId = this.aptIndex;
+          this.aptIndex++;
+          return item
+        }))
+    );
   },
   methods: {
-    removeItem: function(apt) {
-      this.appointments = _.without(this.appointments, apt)
+    removeItem: function (apt) {
+      this.appointments = _.without(this.appointments, apt);
+    },
+    editItem: function(id, field, text){
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id
+      });
+      this.appointments[aptIndex][field] = text;
     }
-  }
+  },
 };
 </script>
 
